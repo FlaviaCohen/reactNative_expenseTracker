c24730ff437d437d36e02a53899240c035ac0fb9
FlaviaCohen <flaviacohen1988@gmail.com>
2023-11-15 11:38:00 -0300

    add env file

--- utils/services.js
+++ utils/services.js
@@ -1,15 +1,14 @@
 import axios from "axios";
-
-const FIREBASE_URL = "https://expensetracker-rn-default-rtdb.firebaseio.com";
+import { DB_URL } from "@env";
 
 export const postExpense = async (expense) => {
-  const response = await axios.post(`${FIREBASE_URL}/expenses.json`, expense);
+  const response = await axios.post(`${DB_URL}/expenses.json`, expense);
   const id = response.data.name;
   return id;
 };
 
 export const getExpenses = async () => {
-  const response = await axios.get(`${FIREBASE_URL}/expenses.json`);
+  const response = await axios.get(`${DB_URL}/expenses.json`);
   const expenses = Object.keys(response.data).map((id) => ({
     id,
     ...response.data[id],
@@ -20,9 +19,9 @@ export const getExpenses = async () => {
 };
 
 export const updateExpense = (id, expense) => {
-  axios.put(`${FIREBASE_URL}/expenses/${id}.json`, expense);
+  axios.put(`${DB_URL}/expenses/${id}.json`, expense);
 };
 
 export const deleteExpense = (id) => {
-  axios.delete(`${FIREBASE_URL}/expenses/${id}.json`);
+  axios.delete(`${DB_URL}/expenses/${id}.json`);
 };
